fc7c7df60dbf6aa71dc4e25fbb2578740e6c4557
jaybat-code <jacobbattenfielddev@gmail.com>
2024-12-03 12:59:09 -0600

    Advent 3 done, learned more about regexes

---
+++ Advent 3/main.py
@@ -0,0 +1,45 @@
+import re
+
+isdo = True
+results = [];
+
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+do_pattern = r"do\(\)"
+dont_pattern = r"don't\(\)"
+
+def mul(a,b):
+    return a*b;
+with open("input.txt","r")as file:
+    for line in file:
+        tokens = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)",line)
+        for token in tokens:
+            if re.match(do_pattern,token):
+                print("Found a do() command, Multiplation Turned On:")
+                isdo = True
+            elif re.match(dont_pattern,token):
+                print("Found a don't() command, Multiplication Turned Off")
+                isdo = False
+            elif re.match(pattern,token):
+                num1,num2=map(int,re.findall(r"\d+",token))
+                if isdo:
+                    result = mul(num1,num2)
+                    print(f"Multiplying {num1} and {num2}")
+                    results.append(result)
+def addresults(results):
+    sum=0;
+    for result in results:
+        sum+=result;
+    return sum;
+print(tokens)
+print(addresults(results))
+
+
+
+
+
+
+
+
+
+
+
